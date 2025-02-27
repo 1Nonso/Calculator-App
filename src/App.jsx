564f1f4a8bda546f54785f43007a4d49c2aa7ac8
Othniel <chukchi2345@gmail.com>
2025-02-27 21:34:09 +0100

    Beginning Calculator Update modifiying navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/nav'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-      </div>
+      <main className='mainApp w-4 bg-yellow'>
+        <Navbar />
+      </main>
     </>
   )
 }
---
+++ src/Components/nav.jsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return <>
+    <section>
+      <p>
+        Calc
+      </p>
+      <div>
+        <div>
+          <p>Themes</p>
+        </div>
+        <div>
+          <div>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
+}
+
+export default Navbar
